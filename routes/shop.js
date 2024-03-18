9fe1a37d587ca3ba429fdfc02208661df9a2d728
asyifa-mg <asyifamaharanigustina12@gmail.com>
2024-03-18 23:25:24 +0700

    initial commit view engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  const product = adminData.products;
+  //res.sendFile(path.join(rootDir, "views", "shop.ejs"));
+  res.render("shop", { title: "FSW 1 LUAR BIASA", products: products });
 });
 
 module.exports = router;
